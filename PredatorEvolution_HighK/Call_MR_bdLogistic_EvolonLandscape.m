8aa7fcac08cf0b43dee4379cbf1b3ccbe0f24451
KyleCoblentz <30963965+KyleCoblentz@users.noreply.github.com>
2023-03-01 16:44:29 -0600

    Add files via upload

---
+++ PredatorEvolution_HighK/Call_MR_bdLogistic_EvolonLandscape.m
@@ -0,0 +1,184 @@
+%%% Rosenzweig-MacArthur BD-Logistic Evolution on Landacape GEM
+
+%%% needs the following 
+    % GEM_MacArthurRosenzweigh
+    % V4_pick_individuals
+    % V4_medians_and_cis
+    % MacArthurRosenzweig_bdLogistic
+
+colors(1:6,1:3) = [[1 0.47 0]; [0.4 0 1]; [0.07 1 0]; [0.9 0 1]; [0.96 1 0]; [1 0 0.2]];
+fill_colors = colors.*0.8;
+    
+% parameters for resource logistic growth
+
+b = 4.5;                      params(1) = b; % prey birth rate
+
+d = 1;                         params(2) = d; % prey death rate
+
+qb = 0.000875;                      params(3) = qb; % birth density dependence
+
+qd = 0.000875;                      params(4) = qd; % death density dependence
+
+% parameters for b, d, qb, and qd give a K of 2000
+
+% parameters for the interaction and predator
+
+a = 0.0125;                       params(5) = a; % space clearance rate
+
+h = 0.05;                        params(6) = h; % handling time
+
+e = 0.1;                        params(7) = e; % conversion efficiency
+
+m = 0.6;                        params(8) = m; % predator per capita mortality
+
+% set up for GEM
+
+cv_vector = [0 0 0 0 0.8 0.4 0 0];
+
+h2_vector = [0 0 0 0 0.6 0.6 0 0];
+
+y0 = [50 100];
+
+num_replicates = 100;
+
+t_max = 50;
+
+tspan = [0 t_max];
+
+% specify which parameters go with which states
+
+state_parameter_match = zeros(length(y0), length(params)); % start matrix with all zeros
+
+state_parameter_match(1, [1:4]) = 1; % parameters associated with R
+
+state_parameter_match(2,[5:8]) = 1; % parameters associated with the predator
+
+% ode to examine how dynamics play out at different parameter values
+
+ode = @(t,y) MacArthurRosenzweig_bdLogistic(t,y,b,d,qb,qd,a,e,h,m);
+
+[t1,y1] = ode45(ode, tspan, y0);
+    figure(1);clf(1);
+    hold on;
+    plot(t1,y1, '-');
+    legend({'R', 'C'});
+    shg;
+
+    averagestart = zeros(1,2);
+    averagestart(1,1:2) = [round(mean(y1(100:113,1))), round(mean(y1(100:113,2)))];
+
+
+% call and run GEM
+
+[x_stand, pop_stand, stand_times, pop_data_out, x_data_out, x_var_data_out] = GEM_MR_bdLogistic(params, state_parameter_match, cv_vector, h2_vector, num_replicates, averagestart, t_max);
+
+% print the number of extant populations at the end of the sampling
+
+num_replicates - sum(isnan(pop_stand(2,end,:)))
+
+% some plots 
+
+% mean population densities
+
+subplot(1,3,1);
+hold on;
+    jbfill(stand_times,pop_data_out(1,:,2),pop_data_out(3,:,2),fill_colors(2,:),'w',1,0.2); hold on;
+    plot(stand_times,pop_data_out(2,:,2),'-','Color',colors(2,:),'LineWidth',2); hold on;        
+    jbfill(stand_times, pop_data_out(1,:,1), pop_data_out(3,:,1), fill_colors(1,:), 'w',1,0.2); hold on;
+    plot(stand_times,pop_data_out(2,:,1),'-','Color',colors(1,:), 'LineWidth',2); hold on;
+    xlabel('Time')
+    ylabel('Density')
+   
+% mean space clearance rate changes
+  
+subplot(1,3,2);
+    hold on;
+    jbfill(stand_times,x_data_out(1,:,5),x_data_out(3,:,5),fill_colors(2,:),'w',1,0.2); hold on;
+    plot(stand_times,x_data_out(2,:,5),'-','Color',colors(2,:),'LineWidth',2); hold on;
+    xlabel('Time')
+    ylabel('Mean Space Clearance Rate')
+
+% mean handling time changes
+
+subplot(1,3,3);
+    hold on;
+    jbfill(stand_times,x_data_out(1,:,6),x_data_out(3,:,6),fill_colors(2,:),'w',1,0.2); hold on;
+    plot(stand_times,x_data_out(2,:,6),'-','Color',colors(2,:),'LineWidth',2); hold on;
+    xlabel('Time')
+    ylabel('Mean Handling Time')
+
+% variance in space clearance rate
+
+subplot(1,3,2);
+    hold on;
+    jbfill(stand_times,x_var_data_out(1,:,5),x_var_data_out(3,:,5),fill_colors(2,:),'w',1,0.2); hold on;
+    plot(stand_times,x_var_data_out(2,:,5),'-','Color',colors(2,:),'LineWidth',2); hold on;
+    xlabel('Time')
+    ylabel('Space Clearance Rate Variance')
+    
+% variance in handling time
+
+subplot(1,3,3);
+    hold on;
+    jbfill(stand_times,x_var_data_out(1,:,6),x_var_data_out(3,:,6),fill_colors(2,:),'w',1,0.2); hold on;
+    plot(stand_times,x_var_data_out(2,:,6),'-','Color',colors(2,:),'LineWidth',2); hold on;
+    xlabel('Time')
+    ylabel('Handling Time Variance')
+ 
+% want to export data
+
+% export median population size data
+
+popmediandata = horzcat(pop_data_out(:,:,1).', pop_data_out(:,:,2).', stand_times.');
+
+popmediandata = mat2dataset(popmediandata, 'VarNames', {'LowerPrey', 'MedPrey', 'UpperPrey','LowerPred', 'MedPred', 'UpperPred', 'Time'});
+
+export(popmediandata, 'File','popmediandata.csv','Delimiter',',')
+
+% export trait median and variance data
+
+mediantraitdata = horzcat(x_data_out(:,:,5).', x_var_data_out(:,:,5).', x_data_out(:,:,6).', x_var_data_out(:,:,6).', stand_times.');
+
+mediantraitdata = mat2dataset(mediantraitdata, 'VarNames', {'lowera', 'meda', 'uppera', 'loweravar', 'medavar', 'upperavar', 'lowerh', 'medh', 'upperh', 'lowerhvar', 'medhvar', 'upperhvar', 'time'});
+
+export(mediantraitdata, 'File', 'mediantraitdata.csv', 'Delimiter', ',');
+
+% individual run data
+
+% individual run population sizes
+
+indpops = reshape(pop_stand, 2, 20100).';  
+
+% individual run space clearance rates and handling times
+
+indtraits = reshape(x_stand(5:6, :, :), 2, 20100).';
+
+% population id
+
+populationnumber = rep(1:100, 201);
+
+% whether each system goes extinct or not
+
+endpop = squeeze(pop_stand(1, end,:));
+
+endpop = isnan(endpop);
+
+endpop = repelem(endpop, 201);
+
+% times for the individual runs
+
+indtimes = repmat(stand_times, 1, 100).';
+
+% put all of the individual run data together and save to a .csv file
+
+indrundata = horzcat(indpops, indtraits, populationnumber, endpop, indtimes);
+
+% convert to dataset 
+
+indrundata = mat2dataset(indrundata, 'VarNames', {'ResDens', 'PredDens', 'SCRate', 'HandlingTime', 'Pop', 'Extinct', 'Time'});
+
+% save as .csv file
+
+export(indrundata, 'File', 'indrundata.csv', 'Delimiter', ',');
+
+
---
+++ PredatorEvolution_HighK/Figure5_Plot.R
@@ -0,0 +1,235 @@
+################################################################################
+### Figure 4 code -- evolution on the landscape
+################################################################################
+
+### load libraries
+
+library(dplyr); library(ggplot2); library(cowplot);library(ggnewscale); library(viridis)
+
+### load data for the landscape
+
+ExtData<- read.csv('ProportionExt_LowIntComp.csv')
+
+################################################################################
+### make heatmap
+################################################################################
+
+ggplot(data = ExtData, aes(x = HandlingTime, y = AttackRate, fill = NumberPersist)) + geom_tile() + 
+  theme_cowplot()
+
+### want to add lines with analytical stability and feasibility boundaries
+
+q <- 0.0005; r <- 3.5; e <- 0.1; m <- 0.6;
+
+feasibility <- function(h, q, e, m) {
+  
+  hout <- h
+  
+  aout <- q*m/((e-h*m))
+  
+  out <- data.frame(h = hout,
+                    a = aout)
+  
+  return(out)
+  
+}
+
+stability <- function(h, q, e, m) {
+  
+  hout <- h
+  
+  aout <- q*(e + h*m)/(h*(e - h*m))
+  
+  out <- data.frame(h = hout,
+                    a = aout)
+  
+  return(out)
+  
+}
+
+
+
+f_high <- feasibility(h = seq(0.005,0.16, length.out = 1000), q = q, e = e, m =m)
+
+s_high <- stability(h = seq(0.005, 0.155, length.out = 1000), q = q, e = e, m = m)
+
+### put them all together
+
+HeatMap_PopPersist <- ggplot(data = ExtData, aes(x = HandlingTime, y = AttackRate)) + geom_tile(aes(fill = NumberPersist)) + 
+  theme_cowplot() + geom_line(data = f_high, aes(x = h, y = a), color = 'white')  + 
+  geom_line(data = s_high, aes(x = h, y = a), color = 'white') + xlab('Handling Time') + 
+  ylab('Space Clearance Rate') + labs(fill = 'Percent Persist') + 
+  theme_cowplot() 
+  # annotate(geom = 'text', x = 0.225, y = 0.1, color = 'white', label = 'Unstable') + 
+  # annotate(geom = 'text', x = 0.4, y = 0.015, color = 'white', label = 'Infeasible')
+
+HeatMap_PopPersist2 <- ggplot(data = ExtData, aes(x = HandlingTime, y = AttackRate)) + geom_tile(aes(fill = NumberPersist)) + 
+  theme_cowplot() + geom_line(data = f_high, aes(x = h, y = a), color = 'white')  + 
+  geom_line(data = s_high, aes(x = h, y = a), color = 'white') + xlab('Handling Time') + 
+  ylab('Space Clearance Rate') + labs(fill = 'Percent Persist')  + theme_cowplot() 
+
+#########################################################################################
+### Individual evolution runs for plot
+#########################################################################################
+
+### load individual run data
+
+inddata <- read.csv('indrundata.csv', na.strings = 'NaN')
+
+### add maximum attack rate points for each run to the heatmap plot
+
+### create dataset that has row containing the mean end of the runs
+
+SCRMeanEnd <- inddata %>% filter(!is.na(SCRate)) %>% group_by(Pop) %>%  slice_tail(., n = 1) %>% group_by(Pop) %>% summarise(SCRate = mean(SCRate, na.rm = TRUE), HandlingTime = mean(HandlingTime, na.rm = TRUE), Extinct = mean(Extinct, na.rm = TRUE))
+
+SCRMeanEnd <- SCRMeanEnd %>% mutate(ExtinctFactor = ifelse(Extinct == 1, 'Extinct', 'Extant'))
+
+SCRStart <- inddata %>% filter(!is.na(SCRate)) %>% group_by(Pop) %>% slice_head(., n = 1) 
+
+### add points to the heat map that are different colors whether the population went extinct or not
+
+ExtinctionHeatMap <- HeatMap_PopPersist2 + new_scale_color() + 
+  geom_point(data = SCRMeanEnd, aes(x = HandlingTime, y = SCRate, shape = ExtinctFactor, color = ExtinctFactor ), size = 1, stroke = 1.25) + 
+  geom_point(aes(x = 0.05, y = 0.0125), size = 3, color = 'white')   + 
+  scale_color_manual(values = c('magenta', 'green'), breaks = c('Extinct', 'Extant'), name = 'Extinct?') + 
+  scale_shape_manual(values = c(4, 16), breaks = c('Extinct', 'Extant'), name = 'Extinct?')
+
+### average trajectories of extinct and extant populations
+
+ExtinctTrajectories <- inddata %>% filter(!is.na(SCRate)) %>% mutate(ExtinctFactor = ifelse(Extinct == 1, "Extinct", "Extant")) %>% 
+  filter(Extinct == 1) %>% group_by(Pop) %>% slice_tail(., n = 1) %>% summarise(SCRate = mean(SCRate), HandlingTime = mean(HandlingTime))
+
+ExtinctStart <- SCRStart %>% filter(Extinct == 1) %>% ungroup() %>% summarise(SCRate = mean(SCRate),  HandlingTime = mean(HandlingTime))
+
+ExtinctTrajectory <- data.frame(SCRate = c(ExtinctStart$SCRate, mean(ExtinctTrajectories$SCRate)),
+                                HandlingTime = c(ExtinctStart$HandlingTime, mean(ExtinctTrajectories$HandlingTime)))
+
+### Extant Trajectories
+
+ExtantTrajectories <- inddata %>% filter(!is.na(SCRate)) %>% mutate(ExtinctFactor = ifelse(Extinct == 1, "Extinct", "Extant")) %>% 
+  filter(Extinct == 0) %>% group_by(Pop) %>% slice_tail(., n = 1) %>% summarise(SCRate = mean(SCRate), HandlingTime = mean(HandlingTime))
+
+ExtantStart <- SCRStart %>% filter(Extinct == 0) %>%  ungroup() %>% summarise(SCRate = mean(SCRate), HandlingTime = mean(HandlingTime))
+
+ExtantTrajectory <- data.frame(SCRate = c(ExtantStart$SCRate, mean(ExtantTrajectories$SCRate)),
+                               HandlingTime = c(ExtantStart$HandlingTime, mean(ExtantTrajectories$HandlingTime)))
+
+### Quantitative Genetics trajectory
+
+### load data
+
+QG_data <- read.csv('TraitDynamics_QG.csv')
+
+##################################################################################################
+### add trajectories to the plots
+##################################################################################################
+
+AverageTrajectoryPlot <- ExtinctionHeatMap + new_scale_color() + 
+ geom_line(data = filter(QG_data, Time < 36), aes(x =HandlingTime, y = SpaceClearanceRate), color = 'white', 
+               arrow = arrow(length = unit(0.03, "npc"), ends = 'first'))
+
+##################################################################################################
+### add mean trait values to the plot
+##################################################################################################
+
+### load trait data
+
+inddata <- inddata %>% mutate(ExtinctFactor = ifelse(Extinct == 1, 'Extinct', 'Extant'))
+
+AvgTrajectory <- inddata  %>% group_by(Time, ExtinctFactor) %>% summarize(SCRate = mean(SCRate, na.rm = TRUE), HandlingTime = mean(HandlingTime, na.rm = TRUE))
+
+SCRatePopulations <- ggplot(data = inddata, aes(x = Time, y = SCRate, group = Pop, color = ExtinctFactor)) + geom_line(alpha = 0.5) + 
+  theme_cowplot() + scale_color_manual(values = c('Extinct' = 'magenta', 'Extant' = 'green'), name = "Extinct?") + ylab('Space Clearance\n Rate') + 
+  geom_line(data = QG_data, aes(x = Time, y = SpaceClearanceRate), inherit.aes = FALSE)
+  
+SCRateAvgPopulations <- ggplot(data = AvgTrajectory, aes(x = Time, y = SCRate, color = ExtinctFactor)) + geom_line(size = 1) +
+  theme_cowplot() + scale_color_manual(values = c('Extinct' = 'magenta', 'Extant' = 'green'), name = 'Extinct?') + ylab('Average Space\n Clearance Rate') +
+  geom_line(data = QG_data, aes(x = Time, y = SpaceClearanceRate), inherit.aes = FALSE)
+
+HandlingTimePopulations <- ggplot(data = inddata, aes(x = Time, y = HandlingTime, group = Pop, color = ExtinctFactor)) + geom_line(alpha = 0.5) + 
+  theme_cowplot() + scale_color_manual(values = c('Extinct' = 'magenta', 'Extant' = 'green'), name = "Extinct?") + ylab('Handling Time') + 
+  geom_line(data = QG_data, aes(x = Time, y = HandlingTime), inherit.aes = FALSE)
+
+HandlingTimeAvgPopulations <- ggplot(data = AvgTrajectory, aes(x = Time, y = HandlingTime, color = ExtinctFactor)) + geom_line(size = 1) +
+  theme_cowplot() + scale_color_manual(values = c('Extinct' = 'magenta', 'Extant' = 'green'), name = 'Extinct?') + ylab('Average \n Handling Time') + 
+  geom_line(data = QG_data, aes(x = Time, y = HandlingTime), inherit.aes = FALSE)
+
+
+#### plots with randomly selected individual runs overlaid onto the heat map
+
+### extinct populations
+
+# which populations went extinct?
+
+which_extinct <- inddata %>% group_by(Pop) %>% filter(Extinct == 1)
+
+which_extinct <- unique(which_extinct$Pop)
+
+rand_extinct <- sample(x = which_extinct, size = 10)
+
+extinct_ex_data <- inddata %>% filter(Pop %in% rand_extinct)
+
+
+### same for extant populations
+
+which_extant <- inddata %>% group_by(Pop) %>% filter(Extinct == 0)
+
+which_extant <- unique(which_extant$Pop)
+
+rand_extant <- sample(x = which_extant, size = 10)
+
+extant_ex_data <- inddata %>% filter(Pop %in% rand_extant)
+
+average_extinct_traj <- extinct_ex_data %>% group_by(Time) %>% summarize(SCRate = mean(SCRate, na.rm = TRUE), HandlingTime = mean(HandlingTime, na.rm = TRUE)) 
+
+average_extant_traj <- extant_ex_data %>% group_by(Time) %>% summarize(SCRate = mean(SCRate, na.rm = TRUE), HandlingTime = mean(HandlingTime, na.rm = TRUE))
+
+Trajectory_Zoomed <- ExtinctionHeatMap + new_scale_color() + geom_line(data = extant_ex_data, aes(x = HandlingTime, y = SCRate, group = Pop), color = 'green', alpha = 0.4) +
+  geom_line(data = extinct_ex_data, aes(x = HandlingTime, y = SCRate, group = Pop), color = 'magenta', alpha = 0.4) + 
+  geom_line(data = average_extinct_traj, aes(x = HandlingTime, y = SCRate), color = 'magenta', size = 1) + 
+  geom_line(data = average_extant_traj, aes(x = HandlingTime, y = SCRate), color = 'green', size = 1) +
+  xlim(c(0, 0.06)) + ylim(c(0,0.06)) +  geom_line(data = filter(QG_data, Time < 36), aes(x =HandlingTime, y = SpaceClearanceRate), color = 'white', 
+                                                  arrow = arrow(length = unit(0.03, "npc"), ends = 'first'))
+
+
+### put plots together
+
+Plots_Together <- plot_grid(AverageTrajectoryPlot, Trajectory_Zoomed, SCRatePopulations, SCRateAvgPopulations,
+                            HandlingTimePopulations, HandlingTimeAvgPopulations, ncol = 2, nrow = 3, rel_heights = c(1,0.8,0.8),
+                            align = 'hv', labels = 'AUTO')
+
+Title <- ggplot() + labs(title = 'K = 2000') + theme(plot.title = element_text(hjust = 0.5, size = 18))
+
+Plots_Together <- plot_grid(Title, Plots_Together, nrow = 2, ncol = 1, rel_heights = c(0.05,1))
+
+
+save_plot(filename = 'Figure_5_HighK.png', plot = Plots_Together, nrow = 2.5, ncol = 2)
+
+#### plots of variances in traits over time
+
+traitdata <- read.csv('mediantraitdata.csv')
+
+# space clearance rate
+
+SCRVarPlot <- ggplot(data = traitdata, aes(x = time, y = medavar)) + geom_line(size = 1) + geom_ribbon(aes(ymin = loweravar, ymax = upperavar), alpha = 0.3) + 
+  theme_cowplot() + xlab('Time') + ylab('Space Clearance Rate Variance')
+
+save_plot(filename = 'SCRVariationPlot.png', SCRVarPlot)
+
+# handling time 
+
+HandlingVarPlot <- ggplot(data = traitdata, aes(x = time, y = medhvar)) + geom_line(size = 1) + geom_ribbon(aes(ymin = lowerhvar, ymax = upperhvar), alpha = 0.3) + 
+  theme_cowplot() + xlab("Time") + ylab("Handling Time Variance")
+
+save_plot(filename = 'HandlingVariationPlot.png', HandlingVarPlot)
+
+### put the two plots together and save as a single file
+
+VarPlots <- plot_grid(SCRVarPlot, HandlingVarPlot, nrow = 1, ncol = 2)
+
+save_plot(filename = 'VarPlots_HighK.png', plot = VarPlots, nrow = 1, ncol = 2, bg = 'white') 
+
+
+
+
+
